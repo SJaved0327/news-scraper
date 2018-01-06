4695456338f51b2fcc636e2df3927de742a9ec84
Sara Javed <sarajaved@Saras-Air.fios-router.home>
2018-01-05 23:19:57 -0500

    articles-div now emptys on load. setting up GET for /articles route

--- public/app.js
+++ public/app.js
@@ -3,6 +3,8 @@
 $("#scrape-button").on("click", function(event){
 	event.preventDefault();
 
+  $("#article-div").empty();
+
 	$.ajax({
     method: "GET",
     url: "/scrape"
@@ -33,5 +35,41 @@ $("#scrape-button").on("click", function(event){
 
 });
 
+/* Saved Articles Button */
 
+$("#scrape-button").on("click", function(event){
+
+  event.preventDefault();
+
+  $("#article-div").empty();
+
+  $.ajax({
+    method: "GET",
+    url: "/articles"
+  })
+    .done(function(data){
+
+      if (data){
+        alert(`You've got ${data.length} epic lists to read!`);
+      }else{
+        alert(`No epic lists today!`);
+      }
+      
+      for(var i = 0; i < data.length; i++){
+        
+        var row = data[i];
+
+        var article = $("<div class='card'>");
+
+        article.append(`<h5 class='card-header'><a target='_blank' href='${data[i].link}'>${data[i].title}</a></h5>`);
+
+        article.append(`<div class='card-body'><p class='card-text'>${data[i].summary}</p></div>`);
+
+        $("#article-div").append(article);
+
+      };
+
+    });
+
+});
 
